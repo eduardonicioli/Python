6d894d9bdb3157ec307267f045eb728859b6cef6
Carlos Eduardo Nicioli de Toledo <111098632+eduardonicioli@users.noreply.github.com>
2023-04-06 20:19:02 -0300

    Media notas

---
+++ NOTAS FATEC.py
@@ -0,0 +1,17 @@
+# NOTAS MATEUS PESO DAS NOTAS SÃO 3, 4, 3, 1
+
+p1 = float(input("DIGITE NOTA P1: "))
+p2 = float(input("DIGITE NOTA P2: "))
+proj = float(input("DIGITE A NOTA PROJETO: "))
+exer = float(input("DIGITE NOTA DOS EXERCÍCIOS: "))
+med = ((p1 * 3) + (p2 * 4) + (proj * 2) + (exer * 1)) / (3 + 4 + 2 + 1)
+print ("NOTA FINAL É: ", med)
+input()
+
+
+#p1 = float(input("DIGITE NOTA P1: "))
+#p2 = float(input("DIGITE NOTA P2: "))
+#proj = float(input("DIGITE NOTA PROJETO: "))
+#med = (p1 + p2 + proj) / 3 #+ 1 #ponto prejeto
+#print ("NOTA FINAL É: ", med)
+#input()
